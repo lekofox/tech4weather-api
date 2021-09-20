89dd41f00b35a4d801f35a1a4e4dbcbbb381d52b
Leandro Dias <leandro_dias9@hotmail.com>
2021-09-20 12:39:02 -0300

    heroku fix

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
-import app from "./app";
-import connection from "./config/connection";
+import app from './app'
+import connection from './config/connection'
 
 const connect = async () => {
   try {
-    await connection.authenticate();
-    console.log("Server running...");
-  } catch (err) {
-    console.log(err);
+    await connection.authenticate()
+    console.log(`Server running on port ${PORT}`)
+  } catch (error) {
+    console.log(error)
   }
-};
+}
+connect()
+
 const PORT = process.env.PORT || 3333
-app.listen(PORT);
+
+app.listen(PORT)
