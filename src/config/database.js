fc228aa425b1e7c92f92e7b67c0d3e566283b4d3
Leandro Dias <79322299+lekofox@users.noreply.github.com>
2021-09-20 13:02:33 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 
 module.exports = {
-  username: "ec2-18-209-143-227.compute-1.amazonaws.com",
+  username: "vmixwpvavnyasx",
   password: "fdeb024512c9458c15fb272c4eee82468e953e8433ef81f5b0933596ecf7c244",
   database: "d2pkbk8n59hd9a",
   host: "ec2-18-209-143-227.compute-1.amazonaws.com",
